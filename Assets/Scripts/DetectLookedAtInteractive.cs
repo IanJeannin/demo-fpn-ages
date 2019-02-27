fb30b382f623c371e664d4033f409ed04b6f1d2e
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2019-02-26 21:55:43 -0600

    Complete Milestone 3 Tutorial 2

--- Assets/Scripts/DetectLookedAtInteractive.cs
+++ Assets/Scripts/DetectLookedAtInteractive.cs
@@ -15,16 +15,33 @@ public class DetectLookedAtInteractive : MonoBehaviour
     [Tooltip("How far from the raycast origin we will search for interactive elements")]
     [SerializeField]
     private float maxRange = 5.0f;
-    
+
+    public IInteractive LookedAtInteractive
+    {
+        get { return lookedAtInteractive; }
+        set { lookedAtInteractive = value; }
+    }
+
+    private IInteractive lookedAtInteractive;
 
     private void FixedUpdate()
     {
         Debug.DrawRay(raycastOrigin.position, raycastOrigin.forward*maxRange, Color.red);
         RaycastHit hitInfo;
         bool objectWasDetected = Physics.Raycast(raycastOrigin.position, raycastOrigin.forward, out hitInfo, maxRange);
+
+        IInteractive interactive = null;
+        LookedAtInteractive = interactive;
+
         if(objectWasDetected)
         {
-            Debug.Log($"Player is looking at: {hitInfo.collider.gameObject.name}");
+            //Debug.Log($"Player is looking at: {hitInfo.collider.gameObject.name}");
+            interactive = hitInfo.collider.GetComponent<IInteractive>();
+        }
+
+        if(interactive!=null)
+        {
+            lookedAtInteractive = interactive;
         }
     }
 }
---
+++ Assets/Scripts/Interfaces/IInteractive.cs
@@ -0,0 +1,7 @@
+﻿/// <summary>
+/// Interface for elements the character can interact with by pressing the interact button
+/// </summary>
+public interface IInteractive
+{
+    void InteractWith();
+}
