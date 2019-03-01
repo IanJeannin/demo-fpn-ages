b9d40556a14483049e6ca435f81215ba6ace6e04
IanJeannin <36770934+IanJeannin@users.noreply.github.com>
2019-02-28 18:11:12 -0600

    Milestone 3 Tutorial 4
    Adding SFX

--- Assets/Scripts/InteractiveObjects.cs
+++ Assets/Scripts/InteractiveObjects.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class InteractiveObjects : MonoBehaviour, IInteractive
 {
     [SerializeField]
     private string displayText = nameof(InteractiveObjects);
 
+   
     public string DisplayText => displayText;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     public void InteractWith()
     {
+        try
+        {
+            audioSource.Play();
+        }
+        catch (System.Exception)
+        {
+
+            throw new System.Exception("Interactive Object requires an audio source component -Is Missing-");
+        }
+        
         Debug.Log($"Player interacted with:  { gameObject.name}");
     }
 }
