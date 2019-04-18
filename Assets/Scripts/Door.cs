095c13cc49603dcbbcf4725ad64fade2887172d5
Ian Jeannin <ian.jeannin@loop.colum.edu>
2019-04-18 17:22:00 -0500

    Revert Master back to how it first was when moving into milestone 4

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -8,8 +8,44 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Door : InteractiveObjects
 {
+    [Tooltip("Assigning a key here will lock the door, if the player has the key in their inventory they can open the locked door.")]
+    [SerializeField]
+    private InventoryObject key;
+    [Tooltip("If this is checked, the associated key will be removed from the players inventory once it is used.")]
+    [SerializeField]
+    private bool consumesKey;
+    [Tooltip("The text that displays when the player looks at the door while it's locked.")]
+    [SerializeField]
+    private string lockedDisplayText= "Locked.";
+    [Tooltip("Play this audio clip when the player activates a locked door without a key.")]
+    [SerializeField]
+    private AudioClip lockedAudioClip;
+    [Tooltip("Play this audio clip when the player activates an unlocked door.")]
+    [SerializeField]
+    private AudioClip openAudioClip;
+
+    public override string DisplayText
+    {
+        get
+        {
+            string toReturn;
+
+            if(isLocked)
+            {
+                toReturn = HasKey ? $"Use: {key.ObjectName}" : lockedDisplayText;
+            }
+            else
+            {
+                toReturn = base.displayText;
+            }
+            return toReturn;
+        }
+    }
+
+    private bool HasKey => PlayerInventory.InventoryObjects.Contains(key);
     private Animator animator;
     private bool isOpen = false;
+    private bool isLocked;
     private int shouldOpenAnimParameter=Animator.StringToHash("shouldOpen");
     /// <summary>
     /// Using a constructor here to initialize displayText in the editor
@@ -23,16 +59,51 @@ public class Door : InteractiveObjects
     {
         base.Awake();
         animator = GetComponent<Animator>();
+        InitializeIsLocked();
+    }
+
+    private void InitializeIsLocked()
+    {
+        if (key != null)
+        {
+            isLocked = true;
+        }
     }
 
     public override void InteractWith()
     {
         if(!isOpen)
         {
-            base.InteractWith();
-            animator.SetBool(shouldOpenAnimParameter, true);
-            displayText = string.Empty;
-            isOpen = true;
+            if(isLocked&&!HasKey)
+            {
+                audioSource.clip = lockedAudioClip;
+            }
+            else //If it's not locked or if it's locked and player does not have a key.
+            {
+                audioSource.clip = openAudioClip;
+                animator.SetBool(shouldOpenAnimParameter, true);
+                displayText = string.Empty;
+                isOpen = true;
+                UnlockDoor();
+            }
+            base.InteractWith(); //This plays a sound effect.
         }
+    }
+
+    private void UnlockDoor()
+    {
+        isLocked = false;
+        if (key != null && consumesKey)
+        {
+            PlayerInventory.InventoryObjects.Remove(key);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audioSource.clip = openAudioClip;
+        animator.SetBool(shouldOpenAnimParameter, true);
+        displayText = string.Empty;
+        isOpen = true;
     }
 }
---
+++ Assets/Scripts/InventoryMenu.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class InventoryMenu : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject inventoryMenuItemTogglePrefab;
+    [Tooltip("Content of the scrollview for the list of inventory items.")]
+    [SerializeField]
+    private Transform inventoryListContentArea;
+    [Tooltip("Place in the UI for displaying the name of the selected inventory item.")]
+    [SerializeField]
+    private Text itemLabelText;
+    [Tooltip("Place in the UI for displaying info about the selected inventory item.")]
+    [SerializeField]
+    private Text descriptionAreaText;
+
+    private static InventoryMenu instance;
+    private CanvasGroup canvasGroup;
+    private RigidbodyFirstPersonController rigidbodyFirstPersonController;
+    private bool isVisible => canvasGroup.alpha > 0;
+    private AudioSource audioSource;
+
+    public static InventoryMenu Instance
+    {
+        get
+        {
+            if (instance==null)
+            {
+                throw new System.Exception("There is currently no InventoryMenu instance but you are trying to access it. Make the InventoryMenu script is applied to a game object in your scene.");
+            }
+            return instance;
+        }
+        private set { instance = value; }
+    }
+
+    public void ExitMenuButtonClicked()
+    {
+        HideMenu();
+    }
+
+    /// <summary>
+    /// Instantiates a new InventoryMenuItemToggle prefab and adds it to the menu.
+    /// </summary>
+    /// <param name="inventoryObjectToAdd"></param>
+    public void AddItemToMenu(InventoryObject inventoryObjectToAdd)
+    {
+        GameObject clone = Instantiate(inventoryMenuItemTogglePrefab, inventoryListContentArea);
+        InventoryMenuItemToggle toggle = clone.GetComponent<InventoryMenuItemToggle>();
+        toggle.AssociatedInventoryObject = inventoryObjectToAdd;
+    }
+
+    private void ShowMenu()
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+        rigidbodyFirstPersonController.enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        audioSource.Play();
+    }
+
+    private void HideMenu()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        rigidbodyFirstPersonController.enabled = true;
+        audioSource.Play();
+    }
+
+    /// <summary>
+    /// Event handler for InventoryMenuItemToggleSelected.
+    /// </summary>
+    private void OnInventoryMenuItemSelected(InventoryObject inventoryObjectThatWasSelected)
+    {
+        itemLabelText.text = inventoryObjectThatWasSelected.ObjectName;
+        descriptionAreaText.text = inventoryObjectThatWasSelected.Description;
+    }
+
+    private void OnEnable()
+    {
+        InventoryMenuItemToggle.InventoryMenuItemSelected += OnInventoryMenuItemSelected;
+    }
+
+    private void OnDisable()
+    {
+        InventoryMenuItemToggle.InventoryMenuItemSelected -= OnInventoryMenuItemSelected;
+    }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetButtonDown("ShowInventory"))
+        {
+            if (isVisible)
+            {
+                HideMenu();
+            }
+            else
+            {
+                ShowMenu();
+            }
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            throw new System.Exception("There is already one instance of InventoryMenu and there can only be one.");
+        }
+        rigidbodyFirstPersonController = FindObjectOfType<RigidbodyFirstPersonController>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        audioSource.volume = 0;
+        HideMenu();
+        audioSource.volume = 1;
+        StartCoroutine(WaitForAudioClip());
+        Debug.Log("We're not done waiting");
+    }
+
+    private IEnumerator WaitForAudioClip()
+    {
+        float originalVolume = audioSource.volume;
+        audioSource.volume = 0;
+        Debug.Log("Start Waiting");
+        yield return new WaitForSeconds(audioSource.clip.length);
+        Debug.Log("StopWaiting");
+        audioSource.volume = originalVolume;
+    }
+}
