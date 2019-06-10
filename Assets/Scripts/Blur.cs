9962224c7e4e644181ff649f02d9d5e895eeb262
Ian Jeannin <ian.jeannin@loop.colum.edu>
2019-06-10 14:17:30 -0400

    Disable Blur function
    Can't get script to turn it off

--- Assets/Scripts/Blur.cs
+++ Assets/Scripts/Blur.cs
@@ -7,12 +7,28 @@ public class Blur : MonoBehaviour
 {
     [SerializeField]
     private PostProcessVolume postProcessVolume;
-
+    /// <summary>
+    /// How long before the blur at the very start of the game goes away. 
+    /// </summary>
+    [SerializeField]
+    private float secondsBeforeBlurFades;
+    //private DepthOfField depthOfField=new DepthOfField();
     private float depthAperture;
-
+    private bool isBlurOn = true;
+    
     private void Start()
     {
+        //DepthOfField depthOfField = new DepthOfField();
+        DepthOfField depthOfField = ScriptableObject.CreateInstance<DepthOfField>();
+        StartCoroutine(BlurDisappear(depthOfField));
+    }
+    
 
+    private IEnumerator BlurDisappear(DepthOfField x)
+    {
+        yield return new WaitForSeconds(secondsBeforeBlurFades);
+        x.aperture.value = 32;
+        //postProcessVolume.GetComponent<DepthOfField>().aperture.value = 32;
     }
 
 }
